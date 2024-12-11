a67317c3466d69ae7e16e8a3d4b5662439cdff94
23002su <23002@supnum.mr>
2024-12-11 09:04:32 +0100

    added books to the list

--- bibiliotheque/bibiliotheque/urls.py
+++ bibiliotheque/bibiliotheque/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +10,7 @@ urlpatterns = [
     path('admin_system/', include('admin_system.urls')),
    
 ]
+
+
+if settings.DEBUG:  # Only serve media in development, not in production!
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
