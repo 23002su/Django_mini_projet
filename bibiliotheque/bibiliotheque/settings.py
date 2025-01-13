55c1d4cade8fff2eace0dfa856283044a489c7fc
23002su <23002@supnum.mr>
2025-01-13 06:46:07 +0100

    added the index page design

--- bibiliotheque/bibiliotheque/settings.py
+++ bibiliotheque/bibiliotheque/settings.py
@@ -108,8 +108,9 @@ USE_TZ = True
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_URL = '/static/'  # URL to access static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
