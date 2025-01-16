f017ae1639a0b1ea7eb8929efd0f6d8fd1896773
mouloud <mouloudissilayine@gmail.com>
2025-01-16 19:28:28 +0100

    crude client et liver

--- bibiliotheque/gestionDeBibiliotheque/urls.py
+++ bibiliotheque/gestionDeBibiliotheque/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("",views.displayBooks,name="displayBooks"),
     path("regester",views.regester,name="regester"),
     path("login",views.login,name="login"),
+    path("deconenction", views.logout, name="logout"),
 ]
--- bibiliotheque/gestionDeBibiliotheque/views.py
+++ bibiliotheque/gestionDeBibiliotheque/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.hashers import make_password,check_password
 from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 from .models import *
 
 # Create your views here.
@@ -18,7 +19,7 @@ def regester(request):
         tel=request.POST['tel']
         age=request.POST['age']
         password=make_password(request.POST['password'])
-        user=Clients(nom=nom,prenom=prenom,email=email,password=password,tel=tel,age=age)
+        user=Clients(nom=nom,prenom=prenom,email=email,password=password,tel=tel,age=age,role='admin')
         user.save()
         return HttpResponse("user est insert ")
 def login(request):
@@ -30,9 +31,18 @@ def login(request):
         user=Clients.objects.filter(email=email).first()
         if user:
             if check_password(password,user.password):
-                return HttpResponse("vrai")
+                if user.role=='user':
+                    request.session['user']='user'
+                    return HttpResponseRedirect(reverse('displayBooks'))
+                else:
+                    request.session['user']='admin'
+                    return HttpResponseRedirect("/admin_system")
             else:
-                return HttpResponse("no")
+                return  render(request,'login.html',{'message':"password n'est pas corecte "})
+                # return HttpResponse("password n'est pas corecte ")
         else:
-            return HttpResponse("no")
-        
+            return  render(request,'login.html',{'message':"user n'est pas enregestre"})
+            # return HttpResponse("user n'est pas enregestre")
+def logout(request):
+    # request.session.flush()
+    return HttpResponse("deconection")
